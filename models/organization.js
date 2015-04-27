d08967f89818621e9148928b1d1f2aa505ad7b6a
Sergey Gaichuk <sergey.gaychuk@gmail.com>
2015-04-27 11:59:44 +0300

    models: add organization model
    Signed-off-by: Sergey Gaichuk <sergey.gaychuk@gmail.com>

---
+++ models/organization.js
@@ -0,0 +1,32 @@
+/* models/organization.js
+ * Copyright 2015 Sergei Ianovich
+ *
+ * Licensed under AGPL-3.0 or later, see LICENSE
+ * Process Control Service Web Interface
+ */
+
+var mongoose = require('mongoose');
+var validates = require('./_validates');
+
+var org_schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validates.length({ max: 50 }),
+      msg: 'name is too long' },
+    trim: true },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true }
+});
+
+org_schema.index({ name: 1 });
+
+var Organization = mongoose.model('Organization', org_schema);
+
+module.exports = Organization;
+
+// vim:ts=2 sts=2 sw=2 et:
+
